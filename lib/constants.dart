597bc25b6c75a1104fb78d7a0f33adbcae0ed676
makmach <471842@ntti.edu.kh>
2025-04-23 17:33:21 +0700

    #1 - Hooking Screen Utils    - Hooking Getx

---
+++ lib/constants.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+String googleApiKey ='';
+
+final kPrimary = Color(0xFF30b9b2);
+final kPrimaryLight = Color(0xFF40F3EA);
+final kSecondary = Color(0xffffa44f);
+final kSecondaryLight = Color(0xFFffe5db);
+final kTertiary = Color(0xff0078a6);
+final kGray = Color(0xff83829A);
+final kGrayLight = Color(0xffC1C0C8);
+final kLightWhite = Color (0xffFAFAFC);
+final kWhite = Color(0xfffFFFFF);
+final kDark = Color(0xFF000000);
+final kRed = Color(0xffe81e4d);
+final kOffWhite = Color(0xffF3F4F8);
+
+double height = 825.h;
+double width = 375.w;
+
+const String appBaseUrl = "http://localhost:6003";
