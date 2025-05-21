2c7edf0c63269214b80c314d2930de0a38403a27
makmach <471842@ntti.edu.kh>
2025-05-21 18:51:20 +0700

    17. Category onTap

--- lib/views/home/widgets/category_list_widget.dart
+++ lib/views/home/widgets/category_list_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import '../../../controllers/category_controller.dart';
+import '../../category/all_categories.dart';
 import '/common/app_style.dart';
 import '/common/reusable_text.dart';
 import '/constants/constants.dart';
@@ -22,33 +23,53 @@ class CategoryListWidget extends StatelessWidget {
           var category = categories[index];
           return GestureDetector(
             onTap: () {
-
+              if (controller.categoryValue == category['_id']) {
+                controller.updateCategory = '';
+                controller.updateTitle = '';
+              } else if (category['value'] == 'more') {
+                Get.to(
+                  () => const AllCategories(),
+                  transition: Transition.fadeIn,
+                  duration: const Duration(milliseconds: 900),
+                );
+              } else {
+                controller.updateCategory = category['_id'];
+                controller.updateTitle = category['title'];
+              }
             },
-            child: Container(
-              margin: EdgeInsets.only(right: 5.w),
-              padding: EdgeInsets.only(top: 4.h),
-              width: width * 0.19,
-              decoration: BoxDecoration(
-                color: kWhite,
-                borderRadius: BorderRadius.circular(10.r),
-                border: Border.all(color: kSecondary, width: .5.w),
-              ),
-              child: Column(
-                children: [
-                  SizedBox(
-                    height: 35.h,
-                    child: Image.asset(
-                      '${category['imageUrl']}',
-                      fit: BoxFit.contain,
+            child: Obx(() {
+              return Container(
+                margin: EdgeInsets.only(right: 5.w),
+                padding: EdgeInsets.only(top: 4.h),
+                width: width * 0.19,
+                decoration: BoxDecoration(
+                  color: kWhite,
+                  borderRadius: BorderRadius.circular(10.r),
+                  border: Border.all(
+                    color:
+                        controller.categoryValue == category['_id']
+                            ? kSecondary
+                            : kOffWhite,
+                    width: 0.5.w,
+                  ),
+                ),
+                child: Column(
+                  children: [
+                    SizedBox(
+                      height: 35.h,
+                      child: Image.asset(
+                        '${category['imageUrl']}',
+                        fit: BoxFit.contain,
+                      ),
                     ),
-                  ),
-                  ReusableText(
-                    text: category['title'],
-                    style: appStyle(12, kDark, FontWeight.normal),
-                  ),
-                ],
-              ),
-            ),
+                    ReusableText(
+                      text: category['title'],
+                      style: appStyle(12, kDark, FontWeight.normal),
+                    ),
+                  ],
+                ),
+              );
+            }),
           );
         }),
       ),
