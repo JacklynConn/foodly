e508cda203c26b7b1c6ea4f0f0be248a69c6914a
makmach <471842@ntti.edu.kh>
2025-05-21 11:58:01 +0700

    5. TabIndex GetXController and Obx

--- lib/views/entrypoint.dart
+++ lib/views/entrypoint.dart
@@ -1,56 +1,56 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-
 import '../constants/constants.dart';
+import 'package:get/get.dart';
+import '../controllers/tab_index_controller.dart';
 
 class MainScreen extends StatelessWidget {
   const MainScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        children: [
-          Container(height: height, width: width, color: kOffWhite),
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: Theme(
-              data: Theme.of(context).copyWith(canvasColor: kPrimary),
-              child: BottomNavigationBar(
-                showSelectedLabels: false,
-                showUnselectedLabels: false,
-                unselectedIconTheme: IconThemeData(
-                  color: Colors.black38
+    final controller = Get.put(TabIndexController());
+    return Obx(() {
+      return Scaffold(
+        body: Stack(
+          children: [
+            Container(height: height, width: width, color: kOffWhite),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Theme(
+                data: Theme.of(context).copyWith(canvasColor: kPrimary),
+                child: BottomNavigationBar(
+                  showSelectedLabels: false,
+                  showUnselectedLabels: false,
+                  unselectedIconTheme: IconThemeData(color: Colors.black38),
+                  selectedIconTheme: IconThemeData(color: kSecondary),
+                  onTap: (value) {
+                    controller.setTabIndex = value;
+                  },
+                  currentIndex: controller.tabIndex,
+                  items: [
+                    BottomNavigationBarItem(
+                      icon: Icon(Icons.home),
+                      label: 'Home',
+                    ),
+                    BottomNavigationBarItem(
+                      icon: Icon(Icons.search),
+                      label: 'Search',
+                    ),
+                    BottomNavigationBarItem(
+                      icon: Icon(Icons.shopping_cart),
+                      label: 'Cart',
+                    ),
+                    BottomNavigationBarItem(
+                      icon: Icon(Icons.person),
+                      label: 'Profile',
+                    ),
+                  ],
                 ),
-                selectedIconTheme: IconThemeData(
-                  color: kSecondary
-                ),
-                onTap: (value) {
-                  print("Selected index: $value");
-                },
-                items: [
-                  BottomNavigationBarItem(
-                    icon: Icon(Icons.home),
-                    label: 'Home',
-                  ),
-                  BottomNavigationBarItem(
-                    icon: Icon(Icons.search),
-                    label: 'Search',
-                  ),
-                  BottomNavigationBarItem(
-                    icon: Icon(Icons.shopping_cart),
-                    label: 'Cart',
-                  ),
-                  BottomNavigationBarItem(
-                    icon: Icon(Icons.person),
-                    label: 'Profile',
-                  ),
-                ],
               ),
             ),
-          ),
-        ],
-      ),
-    );
+          ],
+        ),
+      );
+    });
   }
 }
