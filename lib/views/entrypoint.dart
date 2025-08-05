8245fd5d514e0a8854cdd3c28e52ff175bf423fd
makmach <471842@ntti.edu.kh>
2025-08-05 10:51:15 +0700

    89. SearchBar and CutomTextField

--- lib/views/entrypoint.dart
+++ lib/views/entrypoint.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_vector_icons/flutter_vector_icons.dart';
 import '../views/profile/profile_page.dart';
@@ -48,7 +49,7 @@ class MainScreen extends StatelessWidget {
                       label: 'Home',
                     ),
                     BottomNavigationBarItem(
-                      icon: Icon(Icons.search),
+                      icon: Icon(CupertinoIcons.search),
                       label: 'Search',
                     ),
                     BottomNavigationBarItem(
--- lib/views/search/search_page.dart
+++ lib/views/search/search_page.dart
@@ -1,21 +1,52 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-
+import 'package:flutter_vector_icons/flutter_vector_icons.dart';
 import '../../common/custom_container.dart';
+import '../../common/custom_text_field.dart';
 import '../../constants/constants.dart';
 
-class SearchPage extends StatelessWidget {
+class SearchPage extends StatefulWidget {
   const SearchPage({super.key});
 
   @override
+  State<SearchPage> createState() => _SearchPageState();
+}
+
+class _SearchPageState extends State<SearchPage> {
+  final TextEditingController _searchController = TextEditingController();
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: kPrimary,
-      appBar: PreferredSize(
-        preferredSize: Size.fromHeight(130.h),
-        child: Container(height: 130),
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).unfocus();
+      },
+      child: Scaffold(
+        backgroundColor: kPrimary,
+        appBar: AppBar(
+          toolbarHeight: 74.h,
+          elevation: 0,
+          automaticallyImplyLeading: false,
+          backgroundColor: kOffWhite,
+          title: Padding(
+            padding: EdgeInsets.only(top: 12),
+            child: CustomTextFieldWidget(
+              controller: _searchController,
+              keyboardType: TextInputType.text,
+              hintText: 'Search for Food',
+              contentPadding: EdgeInsets.symmetric(
+                vertical: 14.h,
+                horizontal: 10.w,
+              ),
+              suffixIcon: GestureDetector(
+                onTap: () {},
+                child: Icon(Ionicons.search_circle, size: 40.h, color: kGray),
+              ),
+            ),
+          ),
+        ),
+        body: SafeArea(child: CustomContainer(containerContent: Container())),
       ),
-      body: SafeArea(child: CustomContainer(containerContent: Container())),
     );
   }
 }
