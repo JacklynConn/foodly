d9b61669728933af68e8f60caf9b09b224921982
makmach <471842@ntti.edu.kh>
2025-11-09 11:18:42 +0700

    101. Restaurant Page Placeholder

--- lib/views/food/food_detail_page.dart
+++ lib/views/food/food_detail_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_vector_icons/flutter_vector_icons.dart';
 import 'package:foodly/constants/constants.dart';
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:foodly/views/restaurant/restaurant_page.dart';
 import '../../common/custom_button.dart';
 import '../../controllers/foods_controller.dart';
 import '../../models/food_model.dart';
@@ -28,94 +29,98 @@ class _FoodDetailPageState extends State<FoodDetailPage> {
         physics: NeverScrollableScrollPhysics(),
         padding: EdgeInsets.zero,
         children: [
-          Stack(
-            children: [
-              ClipRRect(
-                borderRadius: BorderRadius.only(
-                  bottomRight: Radius.circular(30.r),
-                ),
-                child: Stack(
-                  children: [
+          ClipRRect(
+            borderRadius: BorderRadius.only(
+              bottomRight: Radius.circular(30.r),
+            ),
+            child: Stack(
+              children: [
 
-                    SizedBox(
-                      height: 230.h,
-                      child: PageView.builder(
-                        controller: pageController,
-                        onPageChanged: (i) {
-                          controller.changPage(i);
-                        },
-                        itemCount: widget.food.imageUrl.length,
-                        itemBuilder: (context, i) {
-                          return Container(
-                            height: 230.h,
-                            width: width,
-                            color: kLightWhite,
-                            child: CachedNetworkImage(
-                              imageUrl: widget.food.imageUrl[i],
-                              fit: BoxFit.cover,
-                            ),
-                          );
-                        },
-                      ),
-                    ),
-
-                    Positioned(
-                      bottom: 10.h,
-                      child: Padding(
-                        padding: const EdgeInsets.only(left: 12.0).w,
-                        child: Obx(() {
-                          return Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: List.generate(
-                              widget.food.imageUrl.length,
-                              (index) {
-                                return Container(
-                                  margin: EdgeInsets.all(4.w),
-                                  height: 10.h,
-                                  width: 10.w,
-                                  decoration: BoxDecoration(
-                                    color:
-                                        controller.currentPage.value == index
-                                            ? kSecondary
-                                            : kGrayLight,
-                                    shape: BoxShape.circle,
-                                  ),
-                                );
-                              },
-                            ),
-                          );
-                        }),
-                      ),
-                    ),
-
-                    Positioned(
-                      top: 40.h,
-                      left: 12.w,
-                      child: GestureDetector(
-                        onTap: () {
-                          Navigator.pop(context);
-                        },
-                        child: Icon(
-                          Ionicons.chevron_back_circle,
-                          color: kPrimary,
-                          size: 30.sp,
+                SizedBox(
+                  height: 230.h,
+                  child: PageView.builder(
+                    controller: pageController,
+                    onPageChanged: (i) {
+                      controller.changPage(i);
+                    },
+                    itemCount: widget.food.imageUrl.length,
+                    itemBuilder: (context, i) {
+                      return Container(
+                        height: 230.h,
+                        width: width,
+                        color: kLightWhite,
+                        child: CachedNetworkImage(
+                          imageUrl: widget.food.imageUrl[i],
+                          fit: BoxFit.cover,
                         ),
-                      ),
-                    ),
-
-                    Positioned(
-                      bottom: 10.h,
-                      right: 12.w,
-                      child: CustomButton(
-                        onTap: () {},
-                        btnWidth: 120.w,
-                        radius: 25.r,
-                        text: 'Open Restaurant',
-                      ),
-                    ),
-                  ],
+                      );
+                    },
+                  ),
                 ),
-              ),
+
+                Positioned(
+                  bottom: 10.h,
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 12.0).w,
+                    child: Obx(() {
+                      return Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: List.generate(
+                          widget.food.imageUrl.length,
+                          (index) {
+                            return Container(
+                              margin: EdgeInsets.all(4.w),
+                              height: 10.h,
+                              width: 10.w,
+                              decoration: BoxDecoration(
+                                color:
+                                    controller.currentPage.value == index
+                                        ? kSecondary
+                                        : kGrayLight,
+                                shape: BoxShape.circle,
+                              ),
+                            );
+                          },
+                        ),
+                      );
+                    }),
+                  ),
+                ),
+
+                Positioned(
+                  top: 40.h,
+                  left: 12.w,
+                  child: GestureDetector(
+                    onTap: () {
+                      Get.back();
+                    },
+                    child: Icon(
+                      Ionicons.chevron_back_circle,
+                      color: kPrimary,
+                      size: 30.sp,
+                    ),
+                  ),
+                ),
+
+                Positioned(
+                  bottom: 10.h,
+                  right: 12.w,
+                  child: CustomButton(
+                    onTap: () {
+                      Get.to(() => RestaurantPage());
+                    },
+                    btnWidth: 120.w,
+                    radius: 25.r,
+                    text: 'Open Restaurant',
+                  ),
+                ),
+              ],
+            ),
+          ),
+
+          Column(
+            children: [
+
             ],
           ),
         ],
