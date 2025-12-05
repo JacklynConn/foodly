a244c7a5c595249ee0cb025b1a4ad68f7a6fd138
makmach <mmach383@gmail.com>
2025-12-05 23:04:35 +0700

    116. Restaurant Page DefaultTabController and Cover

--- lib/views/restaurant/restaurant_page.dart
+++ lib/views/restaurant/restaurant_page.dart
@@ -1,4 +1,7 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:foodly/constants/constants.dart';
 import 'package:foodly/models/restaurants_model.dart';
 
 class RestaurantPage extends StatelessWidget {
@@ -8,10 +11,28 @@ class RestaurantPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    print(restaurant!.coords.address);
-    return Scaffold(
-      appBar: AppBar(),
-      body: Container(),
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        backgroundColor: kLightWhite,
+        body: ListView(
+          padding: .zero,
+          children: [
+            Stack(
+              children: [
+                SizedBox(
+                  height: 230.h,
+                  width: double.infinity,
+                  child: CachedNetworkImage(
+                    imageUrl: restaurant!.imageUrl,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
